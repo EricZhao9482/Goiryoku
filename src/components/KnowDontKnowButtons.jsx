ed6372d72fefe64f4f41874d02cbbdc98dd291c7
EricZhao9482 <ericz39g@gmail.com>
2025-01-18 01:56:45 -0500

    fixed button alignments

--- src/components/KnowDontKnowButtons.jsx
+++ src/components/KnowDontKnowButtons.jsx
@@ -2,13 +2,9 @@
 // eslint-disable-next-line react/prop-types
 function KnowDontKnowButtons( {onHomeScreen} ) {
     return (
-        <div id="kdk-buttons-container" className={` ${onHomeScreen ? "hide" : "fade-in-from-bottom"}`}>
-            <div id="know-button" className={`center btn-primary`}>
-                <p>わかる</p>
-            </div>
-            <div id="dont-know-button" className={`center btn-primary`}>
-                <p>わからない</p>
-            </div>
+        <div id="kdk-buttons-container" className={`center ${onHomeScreen ? "hide" : "fade-in-from-bottom"}`}>
+            <button id="know-button" className="btn-secondary">わかる</button>
+            <button id="dont-know-button" className="btn-secondary">わからない</button>
         </div>
     )
 }
