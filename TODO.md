2e037352c4e97c3fe29bfdb524f76523ee285453
EricZhao9482 <ericz39g@gmail.com>
2025-01-20 18:30:23 -0500

    more styling on the tables

--- TODO.md
+++ TODO.md
@@ -1,6 +1,4 @@
-- Word Lists component has repetitive code where two tables are created next to each other. 
-Make it so that the table becomes its own component that can be resued twice within the WordLists component
 
 - fix edge case where the same word gets generated twice and attempted to be added to the same list in app.jsx
-
-- as js docs to the rest of the components
+- make the definition text has a space in between the commas
+- add js docs to the rest of the components
--- src/components/WordList.jsx
+++ src/components/WordList.jsx
@@ -1,24 +1,34 @@
 /* eslint-disable react/prop-types */
 function WordList( {listName, wordList} ) {
 
+    // constants for css styling
+    const word_col = "word-col";
+    const reading_col = "reading-col";
+    const jlpt_col = "jlpt-col";
     const definition_col = "definition-col";
+
+
+    let listNameFormatting = `${listName}`
+    if (wordList.length > 0) {
+        listNameFormatting += ` (${wordList.length})`;
+    }  
 
     return (
         <div>
-            <p>{listName}</p>
+            <p>{listNameFormatting}</p>
             <table>
                 <tbody>
                     <tr>
-                        <th>言葉</th>
-                        <th>読み方</th>
-                        <th>JLPT</th>
+                        <th className={word_col}>言葉</th>
+                        <th className={reading_col}>読み方</th>
+                        <th className={jlpt_col}>JLPT</th>
                         <th className={definition_col}>英訳</th>
                     </tr>
                     {wordList.map( (word) => (
                         <tr key={word.word}>
-                            <td>{word.word}</td>
-                            <td>{word.reading}</td>
-                            <td>{word.jlptLevel}</td>
+                            <td className={word_col}>{word.word}</td>
+                            <td className={reading_col}>{word.reading}</td>
+                            <td className={jlpt_col}>{word.jlptLevel}</td>
                             <td className={definition_col}>{word.englishMeaning}</td>
                         </tr>
                     ))
