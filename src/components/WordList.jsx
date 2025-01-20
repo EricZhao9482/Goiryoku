c7b1395ea76e276c6d9b9427c17af089705b69f2
EricZhao9482 <ericz39g@gmail.com>
2025-01-20 16:42:09 -0500

    created wordlist component for reuse in wordlists component

---
+++ src/components/WordList.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+function WordList( {ListName, wordList} ) {
+
+    const definition_col = "definition-col";
+
+    return (
+        <div>
+            <p>{ListName}</p>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>言葉</th>
+                        <th>読み方</th>
+                        <th>JLPT</th>
+                        <th className={definition_col}>英訳</th>
+                    </tr>
+                    {wordList.map( (word) => (
+                        <tr key={word.word}>
+                            <td>{word.word}</td>
+                            <td>{word.reading}</td>
+                            <td>{word.jlptLevel}</td>
+                            <td className={definition_col}>{word.englishMeaning}</td>
+                        </tr>
+                    ))
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default WordList;
