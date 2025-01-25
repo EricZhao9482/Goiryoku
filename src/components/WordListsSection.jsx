63ba136164f3f446a851a0f47aadc6f28a0b51c2
EricZhao9482 <ericz39g@gmail.com>
2025-01-24 22:29:13 -0500

    crossed off add jsdocs from todo.md file

--- src/components/WordListsSection.jsx
+++ src/components/WordListsSection.jsx
@@ -2,7 +2,7 @@
 import WordList from "./WordList";
 
 /**
- * 
+ * The section of the app that has the know and dont know word lists
  * @param {boolean} onHomeScreen - lets the component know if its on the homescreen or not to determine visibility
  * @param {Array} knownWordsList - a list of word objects that the user knows 
  * @param {Array} unknownWordsList - a list of word objects that the user doesn't know
